d87021da9ba2e9d795e3c1a755caf8fa4258921c
Ricardo Motta <116051801+Heinenz@users.noreply.github.com>
2024-01-08 11:15:51 -0400

    Add files via upload

---
+++ My_own_madlib.py
@@ -0,0 +1,39 @@
+#Let's try to do some warm ups with this madlib
+#The madlib will be about the daily life of Johnny, a 35-year-old man during winter of last year.
+
+#Inputs for the paragraph/madlib
+
+#Madlib
+
+adjective1 = input("Adjective: ") 
+adjective2 = input("Adjective: ")
+adjective3 = input("Adjective: ") 
+adjective4 = input("Adjective: ")
+adverb1 = input("Adverb: ") 
+noun_plural1 = input("Plural Noun: ")
+verb1 = input("Verb: ") 
+verb2 = input("Verb: ")
+hobby1 = input("Hobby: ")
+hobby2 = input("Hobby: ")
+hobby3 = input("Hobby: ")
+
+madlib = f" Johnny's day begins with the {adjective1} sound of his alarm clock, signaling the start of a new day.\
+As he groggily rises from his bed, he glances at the calendar on his wall, filled with scribbles and reminders of the day's tasks.\
+After a {adverb1} shower and a hearty breakfast, Johnny heads off to his job He works in a {adjective2} office where the hum of computers\
+and the chatter of {noun_plural1} create a lively atmosphere. His tasks vary from day to day, but he approaches each one with diligence \
+and a determination to excel.\
+During his lunch break, Johnny often {verb1} to a nearby park, {verb2} a breath of fresh air \
+and a moment of tranquility  amidst the urban hustle. \
+It's a time for him to recharge and gather his thoughts before diving back into the demands of the afternoon.\
+Once the workday concludes, Johnny may hit the gym for a workout session or meet up with friends for dinner. \
+He values the balance between maintaining a healthy lifestyle and nurturing his social connections. \
+Evenings are sometimes reserved for personal hobbies, whether it's {hobby1}, {hobby2} or {hobby3}.\
+Weekends bring a mix of {adjective3} and {adjective4}. Johnny enjoys exploring new places, trying out different cuisines,\
+or simply unwinding at home with a movie marathon. Occasionally, he volunteers for community events, \
+finding fulfillment in giving back to the neighborhood that has become an integral part of his daily life.\
+As the day comes to a close, Johnny reflects on the challenges and triumphs of the day. \
+With a sense of gratitude, he looks forward to the possibilities the next day holds. \
+His routine may seem ordinary to some, but for Johnny, it's the mosaic of these daily moments that shapes a \
+life filled with purpose and meaning."
+
+print(madlib)
